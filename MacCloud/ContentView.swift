47dd3bd705c882d34e5110dc7d7d53e5ab598030
Iva Horn <iva.horn@nextcloud.com>
2025-11-06 16:22:11 +0100

    Basic user interface mockup.

--- MacCloud/ContentView.swift
+++ MacCloud/ContentView.swift
@@ -1,17 +1,64 @@
 import SwiftUI
 
+enum ServerState {
+    case stopped
+    case starting
+    case running
+    case stopping
+}
+
 struct ContentView: View {
+    @State private var serverState: ServerState
+
+    init(serverState: ServerState = .stopped) {
+        self.serverState = serverState
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            switch serverState {
+                case .stopped:
+                    Text("Nextcloud server is stopped.")
+
+                    Button {
+                        start()
+                    } label: {
+                        Text("Start")
+                    }
+                    
+                case .running:
+                    Text("Nextcloud server is running on [http://localhost:8080](http://localhost:8080).")
+
+                    Button {
+                        stop()
+                    } label: {
+                        Text("Stop")
+                    }
+
+                default:
+                    ProgressView()
+            }
         }
         .padding()
     }
+
+    func start() {}
+
+    func stop() {}
 }
 
-#Preview {
-    ContentView()
+#Preview("Stopped") {
+    ContentView(serverState: .stopped)
+}
+
+#Preview("Starting") {
+    ContentView(serverState: .starting)
+}
+
+#Preview("Running") {
+    ContentView(serverState: .running)
+}
+
+#Preview("Stopping") {
+    ContentView(serverState: .stopping)
 }
