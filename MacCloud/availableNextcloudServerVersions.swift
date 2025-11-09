18fd172047f9c3ab12ccd1762cb279f9ef84ee6c
Iva Horn <iva.horn@nextcloud.com>
2025-11-09 21:03:53 +0100

    Dropped available Nextcloud server version 32.0.0

--- MacCloud/availableNextcloudServerVersions.swift
+++ MacCloud/availableNextcloudServerVersions.swift
@@ -6,6 +6,5 @@
 let availableNextcloudServerVersions = [
     NextcloudServerVersion("30.0.17"),
     NextcloudServerVersion("31.0.10"),
-    NextcloudServerVersion("32.0.0"),
     NextcloudServerVersion("32.0.1")
 ]
