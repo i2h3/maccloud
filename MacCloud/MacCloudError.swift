21e1e7351912eeb23dc1bc19a49f1798bb5b14f2
Iva Horn <iva.horn@nextcloud.com>
2025-12-03 16:23:32 +0100

    Improved logging and Apache handling.

--- MacCloud/MacCloudError.swift
+++ MacCloud/MacCloudError.swift
@@ -2,9 +2,19 @@ import Foundation
 
 enum MacCloudError: Error, LocalizedError {
     ///
+    /// The httpd binary could not be found.
+    ///
+    case missingApache
+
+    ///
     /// The PHP installation could not be found.
     ///
     case missingPHP
+
+    ///
+    /// The unzip binary could not be found.
+    ///
+    case missingUnzip
 
     ///
     /// A programatically launched process terminated with a different status than 0.
@@ -13,8 +23,12 @@ enum MacCloudError: Error, LocalizedError {
 
     var errorDescription: String? {
         switch self {
+            case .missingApache:
+                return "Apache could not be found."
             case .missingPHP:
                 return "PHP could not be found."
+            case .missingUnzip:
+                return "Unzip could not be found."
             case let .processExitStatus(executable, arguments, status):
                 return "\(executable?.path() ?? "nil") \(arguments?.joined(separator: " ") ?? "nil") terminated with status \(status)"
         }
@@ -36,6 +50,8 @@ enum MacCloudError: Error, LocalizedError {
 
     var recoverySuggestion: String? {
         switch self {
+            case .missingApache:
+                return "Install Apache via Homebrew."
             case .missingPHP:
                 return "Install PHP via Homebrew."
             default:
